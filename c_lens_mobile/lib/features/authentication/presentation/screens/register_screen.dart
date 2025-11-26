fba7d7cc47d1b06923d412f7d3ca4888442f4a5e
Yasir Dönmez <yasirdonmez12345@gmail.com>
2025-11-26 19:23:09 +0300

    ekranların kodu temizlendi

--- c_lens_mobile/lib/features/authentication/presentation/screens/register_screen.dart
+++ c_lens_mobile/lib/features/authentication/presentation/screens/register_screen.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:sliding_switch/sliding_switch.dart';
-import '../widgets/register_form.dart';
+import '../widgets/login_form.dart';
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue.shade100,
       appBar: AppBar(
-        title: const Text(
-          'C-Lens',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.blue.shade700,
-        elevation: 0,
+        title: const Text('C-Lens'),
       ),
-      body: const RegisterForm(),
+      body: const LoginForm(),
     );
   }
 }
